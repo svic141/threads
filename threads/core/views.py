27471e2cf9c9636393aaa2edad28d7613eae444a
Sandra Vicente <sandra.vicente@entelect.co.za>
2025-09-17 19:20:34 +0200

    added logins and signups

--- threads/core/views.py
+++ threads/core/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from item.models import Category, Item
+from .forms import SignupForm, LoginForm
 
 # Create your views here.
 def index(request):
@@ -11,3 +12,31 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+        
+    else:
+        form =  SignupForm()
+
+    return render(request, 'core/signup.html', {'form':form})
+
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/index')
+        
+    else:
+        form =  LoginForm()
+
+    return render(request, 'core/login.html', {'form':form})
